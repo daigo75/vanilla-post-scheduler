a9f129f2517175643a1673aba135d7b37a16fcfc
Diego Zanella <businessdad75@gmail.com>
2013-01-21 02:45:17 +0000

    Improved compatibility with Vanilla 2.1

--- PostScheduler/class.postscheduler.plugin.php
+++ PostScheduler/class.postscheduler.plugin.php
@@ -53,7 +53,12 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 	 * @param Controller Sender Sending controller instance.
 	 */
 	public function Base_Render_Before($Sender) {
-		$Sender->AddCssFile($this->GetResource('design/css/postscheduler.css', FALSE, FALSE));
+		$Sender->AddCssFile('postscheduler.css', 'plugins/PostScheduler/design/css');
+
+		// Add the Sprites for some menu items (Vanilla 2.1 only)
+		if(Gdn::PluginManager()->CheckPlugin('Sprites')) {
+			$Sender->AddCssFile('sprites.css', 'plugins/PostScheduler/design/css');
+		}
 	}
 
 	/**
@@ -128,6 +133,7 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 	}
 
 	/**
+	 * Vanilla 2.0 Event Handler.
 	 * Adds a "Scheduled" link to tabs in Index page.
 	 *
 	 * @param Controller Sender Sending controller instance.
@@ -138,13 +144,35 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 		if(!Gdn::Session()->CheckPermission('Plugins.PostScheduler.ScheduleDiscussions')) {
 			return;
 		}
+
+		// TODO Review code to display a Sprite in Vanilla 2.1
 		$CssClass = $Sender->RequestMethod == 'scheduled' ? 'Active' : '';
 		echo Wrap(Anchor(T('Mine (Scheduled)'),
 										 '/discussions/scheduled',
-										 'ScheduledDiscussions TabLink'),
+										 'TabLink'),
 							'li',
-							array('class' => $CssClass)
+							array('class' => 'MyScheduledDiscussions ' . $CssClass)
 						 );
+	}
+
+	/**
+	 * Vanilla 2.1 Event Handler.
+	 * Calls PostSchedulerPlugin::Base_AfterDiscussionTabs_Handler().
+	 *
+	 * @see PostSchedulerPlugin::Base_AfterDiscussionTabs_Handler().
+	 */
+	public function DiscussionsController_AfterDiscussionFilters_Handler($Sender) {
+		return $this->Base_AfterDiscussionTabs_Handler($Sender);
+	}
+
+	/**
+	 * Vanilla 2.1 Event Handler.
+	 * Calls PostSchedulerPlugin::Base_AfterDiscussionTabs_Handler().
+	 *
+	 * @see PostSchedulerPlugin::Base_AfterDiscussionTabs_Handler().
+	 */
+	public function DiscussionController_AfterDiscussionFilters_Handler($Sender) {
+		return $this->Base_AfterDiscussionTabs_Handler($Sender);
 	}
 
 	/**
@@ -157,9 +185,9 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 		$Now = date('Y-m-d H:i:s');
 		$Sender->SQL
 			->BeginWhereGroup()
-			->Where('InsertUserID', Gdn::Session()->UserID)
-			->Where('Scheduled', self::SCHEDULED_YES)
-			->Where('ScheduleTime >', $Now)
+			->Where('d.InsertUserID', Gdn::Session()->UserID)
+			->Where('d.Scheduled', self::SCHEDULED_YES)
+			->Where('d.ScheduleTime >', $Now)
 			->EndWhereGroup();
 	}
 
@@ -173,19 +201,20 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 		$Now = date('Y-m-d H:i:s');
 		$Sender->SQL
 			->BeginWhereGroup()
-			->Where('InsertUserID', Gdn::Session()->UserID)
-			->OrWhere('Scheduled', null)
-			->OrWhere('ScheduleTime <=', $Now)
+			->Where('d.InsertUserID', Gdn::Session()->UserID)
+			->OrWhere('d.Scheduled', null)
+			->OrWhere('d.Scheduled', 0)
+			->OrWhere('d.ScheduleTime <=', $Now)
 			->EndWhereGroup();
 	}
 
 	/**
-	 * Handler of Event Base::DiscussionMeta.
+	 * Vanilla 2.0 Handler.
 	 * Displays schedule information for scheduled discussions.
 	 *
 	 * @param Controller Sender Sending controller instance.
 	 */
-	public function Base_DiscussionMeta_Handler($Sender) {
+	public function DiscussionsController_DiscussionMeta_Handler($Sender) {
 		$Discussion = &$Sender->EventArguments['Discussion'];
 
 		$Now = date('Y-m-d H:i:s');
@@ -198,6 +227,33 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 
 			//var_dump($Discussion->ScheduleTime);
 		}
+	}
+
+	/**
+	 * Vanilla 2.1 Event Handler.
+	 * Calls PostSchedulerPlugin::DiscussionsController_DiscussionMeta_Handler().
+	 *
+	 * @see PostSchedulerPlugin::DiscussionsController_DiscussionMeta_Handler().
+	 */
+	public function DiscussionController_AfterDiscussionBody_Handler($Sender) {
+		return $this->DiscussionsController_DiscussionMeta_Handler($Sender);
+	}
+
+	/**
+	 * Vanilla 2.1 Event Handler.
+	 * Calls PostSchedulerPlugin::DiscussionsController_DiscussionMeta_Handler().
+	 *
+	 * @see PostSchedulerPlugin::DiscussionsController_DiscussionMeta_Handler().
+	 */
+	public function DiscussionsController_AfterDiscussionLabels_Handler($Sender) {
+		/* This event doesn't need to be handled on My Discussions and My Scheduled
+		 * Discussions pages, as they already fire the
+		 * DiscussionsController_DiscussionMeta event, which performs the same tasks.
+		 */
+		if(in_array(Gdn::Controller()->RequestMethod, array('mine', 'scheduled'))) {
+			return;
+		}
+		return $this->DiscussionsController_DiscussionMeta_Handler($Sender);
 	}
 
 	/**
@@ -240,15 +296,19 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 		}
   }
 
+	/**
+	 * Loads the CSS and JavaScript files required by the plugin.
+	 */
 	public function PostController_BeforeFormInputs_Handler($Sender) {
 		//$Sender->AddJsFile('jquery.ui.packed.js');
 		$UITheme = C('Plugin.PostScheduler.UITheme', self::DEFAULT_UI_THEME);
 
-		$Sender->AddCssFile($this->GetResource('design/jquery-ui/' . $UITheme . '/jquery-ui-1.10.0.custom.min.css', FALSE, FALSE));
+		$Sender->AddCssFile('jquery-ui-1.10.0.custom.min.css', 'plugins/PostScheduler/design/jquery-ui/' . $UITheme);
 
-		$Sender->AddJsFile($this->GetResource('js/jquery-ui-1.10.0.custom.min.js', FALSE, FALSE));
-		$Sender->AddJsFile($this->GetResource('js/jquery-ui-timepicker-addon.js', FALSE, FALSE));
-		$Sender->AddJsFile($this->GetResource('js/postscheduler.js', FALSE, FALSE));
+		$Sender->RemoveJsFile('jquery-ui-1.8.17.custom.min.js');
+		$Sender->AddJsFile('jquery-ui-1.10.0.custom.min.js', 'plugins/PostScheduler/js');
+		$Sender->AddJsFile('jquery-ui-timepicker-addon.js', 'plugins/PostScheduler/js');
+		$Sender->AddJsFile('postscheduler.js', 'plugins/PostScheduler/js');
 
 		//var_dump($Sender);die();
 	}
@@ -298,7 +358,7 @@ class PostSchedulerPlugin extends Gdn_Plugin {
 
 		$Now = date('Y-m-d H:i:s');
 		if($Discussion->ScheduleTime > $Now) {
-			$Sender->EventArguments['CssClass'] .= ' NotVisible';
+			$Sender->EventArguments['CssClass'] .= ' NotYetVisible';
 		}
 	}
 
