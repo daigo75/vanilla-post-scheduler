f51e916bd3936fe04a3d038a8b8fcf304880edcf
Diego Zanella <businessdad75@gmail.com>
2013-02-23 15:16:49 +0000

    Added indexes on schedule fields for Discussion and Activity tables

--- PostScheduler/install/postscheduler.schema.php
+++ PostScheduler/install/postscheduler.schema.php
@@ -19,13 +19,55 @@ class PostSchedulerSchema extends PluginSchema {
 			->Column('Scheduled', 'smallint', 0)
 			->Column('ScheduleTime', 'datetime', true)
 			->Set();
+
+		$this->CreateIndex('Discussion',
+											 'IX_DiscussionSchedule',
+											 array('Scheduled', 'ScheduleTime'),
+											 '');
 	}
 
 	/**
 	 * Drops the columns that will indicate if a Discussion is scheduled.
 	 */
 	private function DropScheduledDiscussionColumns() {
+		$this->DropIndex('Discussion', 'IX_DiscussionSchedule');
+
 		Gdn::Structure()->Table('Discussion');
+
+		// Drop "Scheduled" column, if it exists
+		if(Gdn::Structure()->ColumnExists('Scheduled')) {
+			Gdn::Structure()->DropColumn('Scheduled');
+		}
+		// Drop "ScheduleTime" column, if it exists
+		if(Gdn::Structure()->ColumnExists('ScheduleTime')) {
+			Gdn::Structure()->DropColumn('ScheduleTime');
+		}
+	}
+
+	/**
+	 * Adds the columns that will indicate if an Activity Notification is scheduled.
+	 */
+	private function AddScheduledActivityNotificationColumns() {
+		Gdn::Structure()->Table('Activity');
+
+		Gdn::Structure()
+			->Column('Scheduled', 'smallint', 0)
+			->Column('ScheduleTime', 'datetime', true)
+			->Set();
+
+		$this->CreateIndex('Activity',
+											 'IX_ActivitySchedule',
+											 array('Scheduled', 'ScheduleTime'),
+											 '');
+	}
+
+	/**
+	 * Drops the columns that will indicate if a Discussion is scheduled.
+	 */
+	private function DropScheduledActivityNotificationColumns() {
+		$this->DropIndex('Discussion', 'IX_ActivitySchedule');
+
+		Gdn::Structure()->Table('Activity');
 
 		// Drop "Scheduled" column, if it exists
 		if(Gdn::Structure()->ColumnExists('Scheduled')) {
@@ -42,6 +84,7 @@ class PostSchedulerSchema extends PluginSchema {
 	 */
 	protected function CreateObjects() {
 		$this->AddScheduledDiscussionColumns();
+		$this->AddScheduledActivityNotificationColumns();
 	}
 
 	/**
@@ -49,5 +92,6 @@ class PostSchedulerSchema extends PluginSchema {
 	 */
 	protected function DropObjects() {
 		$this->DropScheduledDiscussionColumns();
+		$this->DropScheduledActivityNotificationColumns();
 	}
 }
