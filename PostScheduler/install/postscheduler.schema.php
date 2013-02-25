e73caf1eced059af5e894f3bade5fb560827212b
Diego Zanella <businessdad75@gmail.com>
2013-02-25 20:45:26 +0000

    Added compatibility with Vanilla 2.1b1

--- PostScheduler/install/postscheduler.schema.php
+++ PostScheduler/install/postscheduler.schema.php
@@ -9,6 +9,18 @@ require('plugin.schema.php');
  * Handle schema changes.
  */
 class PostSchedulerSchema extends PluginSchema {
+	// @var string Stores the version of Vanilla. Used to determine which operations to perform
+	private $_VanillaVersion;
+
+	public function __construct() {
+		parent::__construct();
+
+		// Split Vanilla version into its parts (major version, minor version, release, build)
+		$VanillaVersionParts = explode('.', APPLICATION_VERSION);
+		// Obtain Vanilla version without the period (i.e. 2.0 becomes 20)
+		$this->_VanillaVersion = $VanillaVersionParts[0] . $VanillaVersionParts[1];
+	}
+
 	/**
 	 * Adds the columns that will indicate if a Discussion is scheduled.
 	 */
@@ -50,6 +62,18 @@ class PostSchedulerSchema extends PluginSchema {
 	private function AddScheduledActivityNotificationColumns() {
 		Gdn::Structure()->Table('Activity');
 
+		$Method = 'AddActivityColumns_Vanilla' . $this->_VanillaVersion;
+		if(method_exists($this, $Method)) {
+			$this->$Method();
+		}
+	}
+
+	/**
+	 * Alters the Activity table on a Vanilla 2.0 installation.
+	 *
+	 * @see PostSchedulerSchema::AddScheduledActivityNotificationColumns().
+	 */
+	private function AddActivityColumns_Vanilla20() {
 		Gdn::Structure()
 			->Column('NotificationSent', 'smallint', 1)
 			->Column('DiscussionID', 'smallint', true)
@@ -66,6 +90,22 @@ class PostSchedulerSchema extends PluginSchema {
 	}
 
 	/**
+	 * Alters the Activity table on a Vanilla 2.1b1 installation.
+	 *
+	 * @see PostSchedulerSchema::AddScheduledActivityNotificationColumns().
+	 */
+	private function AddActivityColumns_Vanilla21b1() {
+		$this->CreateIndex('Activity',
+											 'IX_ActivityNotificationSent',
+											 array('Emailed'),
+											 '');
+		$this->CreateIndex('Activity',
+											 'IX_ActivityDiscussionID',
+											 array('RecordType', 'RecordID'),
+											 '');
+	}
+
+	/**
 	 * Drops the columns that will indicate if a Discussion is scheduled.
 	 */
 	private function DropScheduledActivityNotificationColumns() {
@@ -74,7 +114,19 @@ class PostSchedulerSchema extends PluginSchema {
 
 		Gdn::Structure()->Table('Activity');
 
-		// Drop "ScheduleTime" column, if it exists
+		$Method = 'DropActivityColumns_Vanilla' . $this->_VanillaVersion;
+		if(method_exists($this, $Method)) {
+			$this->$Method();
+		}
+	}
+
+	/**
+	 * Drops columns added to Activity table on a Vanilla 2.0 installation.
+	 *
+	 * @see PostSchedulerSchema::DropScheduledActivityNotificationColumns().
+	 */
+	private function DropActivityColumns_Vanilla20() {
+		// Drop "NotificationSent" column, if it exists
 		if(Gdn::Structure()->ColumnExists('NotificationSent')) {
 			Gdn::Structure()->DropColumn('NotificationSent');
 		}
@@ -83,6 +135,15 @@ class PostSchedulerSchema extends PluginSchema {
 		if(Gdn::Structure()->ColumnExists('DiscussionID')) {
 			Gdn::Structure()->DropColumn('DiscussionID');
 		}
+	}
+
+	/**
+	 * Drops columns added to Activity table on a Vanilla 2.1b1 installation.
+	 *
+	 * @see PostSchedulerSchema::DropScheduledActivityNotificationColumns().
+	 */
+	private function DropActivityColumns_Vanilla21b1() {
+		// Nothing to do for Vanilla 2.1b1
 	}
 
 	/**
